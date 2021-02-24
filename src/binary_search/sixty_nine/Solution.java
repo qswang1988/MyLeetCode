3ea6f52ab7c8160a914fa40aeeac161460720882
qswang1988 <qswang1988@gmail.com>
2021-02-24 05:30:09 +0100

    update

--- src/binary_search/sixty_nine/Solution.java
+++ src/binary_search/sixty_nine/Solution.java
@@ -13,25 +13,18 @@ public class Solution {
             return 1;
         long left = 0;
         long right = x/2+1;
-        //int i = 0;
 
         while(true){
-            //i++;
-            long half = left + (right-left)/2;
+            long half = (right+left)/2;
             long product = half*half;
             //System.out.println("left: "+left+",right: "+right+",half: "+half+",product of half: "+product);
-            if(product == x)
+             if(product == x)
                 return (int)half;
             else if(product>x){
                 right = half;
             }else{
-
-                if(left==half) {
-                    /*if(left==right-1)
-                        return right;
-                    else*/
-                        return (int)left;
-                }
+                if(left==half)
+                    return (int)left;
                 left = half;
             }
             continue;
