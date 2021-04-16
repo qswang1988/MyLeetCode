35ab3f476c8aaef9168bfad5caf29befbad3d7af
qswang1988 <qswang1988@gmail.com>
2021-04-16 02:48:53 +0200

    update problem 79. "Word Search, 1ms". Need more optimization later..

--- src/search/seventyNine/Solution.java
+++ src/search/seventyNine/Solution.java
@@ -7,6 +7,8 @@ public class Solution {
         char [][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
         char [][] board2 = {{'A','B','C','E'},{'S','F','E','S'},{'A','D','E','E'}};
 
+        System.out.println(Arrays.deepToString(board));
+
         String word = "ABCB";
         String word2 = "ABCESEEEFS";
         boolean r = new Solution().exist(board2,word2);
@@ -14,13 +16,24 @@ public class Solution {
     }
 
     public boolean exist(char[][] board, String word) {
+
+
+        // if any char of word doesn't exist in the matrix, then we fail the function directly
+        // this can save a lot of time consumption for this test
+        String board_str = Arrays.deepToString(board);
+        for(char s:word.toCharArray()){
+            if(board_str.indexOf(s)==-1)
+                return false;
+        }
+
         char [] word_char = word.toCharArray();
-        int [][] map = new int [board.length][board[0].length];
         for(int row = 0;row<board.length;row++){
             for(int col = 0;col<board[0].length;col++){
-                if(board[row][col] == word_char[0])
-                    if(search(board,word_char,map,0,row,col))
+                int [][] map = new int [board.length][board[0].length];
+                if(board[row][col] == word_char[0]) {
+                    if (search(board, word_char, map, 0, row, col))
                         return true;
+                }
             }
         }
         return false;
@@ -43,12 +56,12 @@ public class Solution {
         boolean right_r = (right==target&&map[row][col+1]==0)?search(board,word,map,pos+1,row,col+1):false;
         boolean down_r = (down==target&&map[row+1][col]==0)?search(board,word,map,pos+1,row+1,col):false;
 
-        if(!(top_r||left_r||right_r||down_r)) {
-            // revert the map, it's very important
-            map[row][col] = 0;
-            return false;
-        }else {
+        if(top_r||left_r||right_r||down_r)
             return true;
-        }
+
+        // revert the map, it's very important
+        map[row][col] = 0;
+        return false;
+
     }
 }
